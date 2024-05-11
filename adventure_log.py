7af84a4e748617ed25686f31f3340b15364d40b3
sangsioco <sangsioco@gmail.com>
2024-05-10 20:50:40 -0700

    Updated Python file

--- adventure_log.py
+++ adventure_log.py
@@ -1,6 +1,6 @@
 def greeting_coding_temple():
-    print ("Ahoy, Coding Temple Navigators! Ready for today's adventure?")
+    print ("Hello, Coding Temple Navigators!")
 
 if __name__ == "__main__":
     greeting_coding_temple()
-    
+
